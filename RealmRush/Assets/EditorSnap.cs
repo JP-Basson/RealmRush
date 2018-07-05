cf8d4c4fe5a22c08afe67e63b5969c898ff0bbd6
jp.basson <jp.basson>
2018-07-05 12:34:47 +0200

    updated editorsnap to clamp the position of cube when moving cubes around

--- RealmRush/Assets/EditorSnap.cs
+++ RealmRush/Assets/EditorSnap.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class EditorSnap : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+[SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
 	// Update is called once per frame
 	void Update () {
-		
+
+		Vector3 snapPos;
+
+		snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+		snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+		transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
 	}
 }
